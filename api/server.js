a08a3624cbd1d4debc91e4ee38c4729ab995a570
Gia May <gia.may@wwt.com>
2022-08-11 14:35:13 -0400

    Routes working

--- api/server.js
+++ api/server.js
@@ -1,17 +1,19 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const workoutRoutes = require('./routes/workouts');
 const journalRoutes = require('./routes/journals');
 const goalRoutes = require('./routes/goals');
 
-
 // express app
 const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
