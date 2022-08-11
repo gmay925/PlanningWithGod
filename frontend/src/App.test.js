5da4986098e3b34a1b32ef52c1c73f269707f4b0
Gia May <gia.may@wwt.com>
2022-08-11 15:32:48 -0400

    Add installs

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { Routes, Route } from 'react-router-dom';
+import { screen, waitFor } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should navigate to log in if non-authorized user attempts to go to preferences', async () => {
+    localStorage.removeItem('loggedIn');
+
+    await renderAndWait(
+      <Routes>
+        <Route exact path="/preferences" element={<AuthRoute><div>authenticated</div></AuthRoute>} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>,
+      '/preferences',
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText('authenticated')).not.toBeInTheDocument();
+    });
+  });
 });
